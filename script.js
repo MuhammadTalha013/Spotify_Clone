c587e7a21a0dafdcddd3e2c205f3569a396f505a
Muhammad Talha <153989042+MuhammadTalha013@users.noreply.github.com>
2024-09-23 00:17:46 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+let currentSong = new Audio();
+
+function convertSecondsToMMSS(seconds) {
+  // Ensure the input is a non-negative integer
+  if (isNaN(seconds) || seconds<0) {
+    return "Invalid Input"
+    }
+
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = Math.floor (seconds % 60);
+
+  // Format minutes and seconds with leading zeros
+  const formattedMinutes = String(minutes).padStart(2, '0');
+  const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+
+  return `${formattedMinutes}:${formattedSeconds}`;
+  }
 async function getSongs() {
   let a = await fetch("http://127.0.0.1:3000/songs/");
   let response = await a.text();
@@ -5,15 +22,70 @@ async function getSongs() {
   let div = document.createElement("div");
   div.innerHTML = response;
   let as = div.getElementsByTagName("a");
-  //   console.log(as); //It gives HTML collection
+  // console.log(as); //It gives HTML collection
   let songs = [];
   for (let index = 0; index < as.length; index++) {
     const element = as[index];
     if (element.href.endsWith(".mp3")) {
-      songs.push(element.href);
+      songs.push(element.href.split("/songs/")[1]);
     }
   }
   return songs;
 }
-let songs = getSongs();
-console.log(songs)
+
+const playMusic = (track) => {
+  currentSong.src = "/songs/" + track;
+  currentSong.play();
+  play.src = "pause.svg"
+  document.querySelector('.songinfo').innerHTML = track 
+  document.querySelector('.songtime').innerHTML = "00:00 / 00:00"
+};
+
+async function main() {
+  //Get the list of all songs
+  let songs = await getSongs();
+  //Show all the songs in the playlist
+  let songUL = document
+    .querySelector(".songList")
+    .getElementsByTagName("ul")[0];
+  for (const song of songs) {
+    songUL.innerHTML =
+      songUL.innerHTML +
+      `<li><img class="invert" src="music.svg" alt="">
+                <div class="info">
+                  <div>${song.replaceAll("%20", " ")}</div>
+                  <div>Mallu</div>
+                </div>
+                <div class="playnow">
+                  <span>Play Now</span>
+                  <img src="play.svg" alt="" class="invert">
+                </div></li>`;
+  }
+  //Attach an event listener to each song
+  Array.from(
+    document.querySelector(".songList").getElementsByTagName("li")
+  ).forEach((e) => {
+    e.addEventListener("click", (element) => {
+      console.log(e.querySelector(".info").firstElementChild.innerHTML);
+      playMusic(e.querySelector(".info").firstElementChild.innerHTML);
+    });
+  });
+  //Attach an event listener to play, next & previous
+  play.addEventListener('click', ()=>{
+    if (currentSong.paused) {
+      currentSong.play()
+      play.src = 'pause.svg'
+    }
+    else{
+      currentSong.pause()
+      play.src = 'play.svg'
+    }
+  })
+
+  //Listen for timeupdate event
+  currentSong.addEventListener('timeupdate', ()=>{
+    console.log(currentSong.currentTime, currentSong.duration)
+    document.querySelector('.songtime').innerHTML = `${convertSecondsToMMSS(currentSong.currentTime)}/${convertSecondsToMMSS(currentSong.duration)}`
+  })
+}
+main();
