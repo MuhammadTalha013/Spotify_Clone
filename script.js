daea48dcc741342828c8ef030c06ef4f3f37ee61
Muhammad Talha <153989042+MuhammadTalha013@users.noreply.github.com>
2024-09-24 22:53:27 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -97,6 +97,10 @@ async function main() {
   document.querySelector(".hamburger").addEventListener("click", ()=>{
     document.querySelector(".left").style.left = "0"
   })
+  //Add an event listener for close button
+  document.querySelector(".close").addEventListener("click", ()=>{
+    document.querySelector(".left").style.left = "-120%"
+  })
 
 
 }
